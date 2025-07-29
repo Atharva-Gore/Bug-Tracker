5760935dc2d3100791506a4496b7bf4c71c2f779
Atharva Gore <atharvagore033@gmail.com>
2025-07-29 13:43:55 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import BugDetails from './pages/BugDetails';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/bug/:id" element={<BugDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
